e6000a920e680f9e55ca0772ed61e493bb98cb2c
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2020-05-16 10:24:50 +0200

    add Layout component

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,37 +1,18 @@
-import React, { useState, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import clsx from 'clsx'
 
-import {
-  CssBaseline,
-  Drawer,
-  Box,
-  AppBar,
-  Toolbar,
-  Typography,
-  Divider,
-  IconButton,
-  // Badge,
-  Container,
-  Grid,
-  Paper,
-} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-// import NotificationsIcon from '@material-ui/icons/Notifications'
-import { MainListItems } from './listItems'
+import { Container, Grid, Paper } from '@material-ui/core'
 // import Chart from './Chart';
 // import Deposits from './Deposits';
 // import Orders from './Orders';
+
+import { Layout } from '../../components/Layout'
 import { Articles } from '../../components/Articles'
 
-import { Copyright } from '../../components/Copyright'
-
 import { useStyles } from './styled'
-// import { schema } from './schema'
 
 const Dashboard = () => {
   const classes = useStyles()
-  const [open, setOpen] = useState(true)
 
   const fixedHeightPaper = useMemo(
     () => clsx(classes.paper, classes.fixedHeight),
@@ -39,99 +20,33 @@ const Dashboard = () => {
   )
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="absolute"
-        className={clsx(classes.appBar, open && classes.appBarShift)}
-      >
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={() => {
-              setOpen(true)
-            }}
-            className={clsx(
-              classes.menuButton,
-              open && classes.menuButtonHidden
-            )}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            className={classes.title}
-          >
-            Dashboard
-          </Typography>
-          {/* <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton> */}
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <Typography variant="h4" component="h1">
-            PHM CUP
-          </Typography>
-          <IconButton
-            onClick={() => {
-              setOpen(false)
-            }}
-          >
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <MainListItems />
-        {/*
-        <List>{secondaryListItems}</List> */}
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                {/* <Chart /> */}
-                chart
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                {/* <Deposits /> */}
-                deposits
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              {/* <Paper className={classes.paper}> */}
-              {/* <Orders /> */}
-              <Articles />
-              {/* </Paper> */}
-            </Grid>
+    <Layout>
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper className={fixedHeightPaper}>
+              {/* <Chart /> */}
+              chart
+            </Paper>
           </Grid>
-          <Box pt={4}>
-            <Copyright />
-          </Box>
-        </Container>
-      </main>
-    </div>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper className={fixedHeightPaper}>
+              {/* <Deposits /> */}
+              deposits
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            {/* <Paper className={classes.paper}> */}
+            {/* <Orders /> */}
+            <Articles />
+            {/* </Paper> */}
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
   )
 }
 
