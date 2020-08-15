7045db3f52563d3d3b2b44fbcc3f0c0619a36968
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2020-08-15 15:53:46 +0200

    Articles component styles

---
+++ src/components/Articles/styled.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
+export { useStyles }
