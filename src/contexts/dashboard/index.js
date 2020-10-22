10998adb57101be17381462904abdc82237c1f94
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2020-10-22 15:04:52 +0200

    update dashboard context

--- src/contexts/dashboard/index.js
+++ src/contexts/dashboard/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 const initialContextState = {
   newArticleCreated: false,
   newTeamsImported: false,
+  newPlayersImported: false,
 }
 const DashboardContext = React.createContext(initialContextState)
 export { initialContextState, DashboardContext as default }
