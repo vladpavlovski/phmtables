a78d1d11fc5c9f775fa58ec69cfa3d94c1036cc2
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2020-10-27 09:49:43 +0100

    hot loader

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
+import 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { App } from './App'
+
+import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-const render = () =>
-  ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById('root')
-  )
-
-if (module.hot) {
-  module.hot.accept('./App', render)
-}
+const render = () => ReactDOM.render(<App />, document.getElementById('root'))
 
 render()
+
+// if (module.hot) {
+//   module.hot.accept('./App', render)
+// }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
